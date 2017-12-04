6e6b9913cc1bd3c750e8ae378ba408a63e5c7c2c
graci <graci@172.28.0.229>
2017-12-04 08:46:42 -0400

    Initial Commit

---
+++ templates/logout.php
@@ -0,0 +1,16 @@
+<!-- Logout Page Template Content -->
+<div class="container">
+    <h1 class="mt-4 mb-3">Logout</h1>
+
+    <!-- mwilliams:  breadcrumb navigation -->
+    <ol class="breadcrumb">
+        <li class="breadcrumb-item"><a href="index.php">Home</a></li>
+        <li class="breadcrumb-item active">Logout</li>            
+    </ol>
+    <!-- end breadcrumb -->
+    <div class="alert alert-success" role="alert">
+        Thank you for visiting.  You are now logged out! <br>
+        Please come back soon!
+    </div>
+</div>
+<!-- /.container -->
